c191487e8d697c70f0418e531845a811d5b8b00f
MKSisti <mohammedkarimsisti@gmail.com>
2021-02-10 20:21:15 +0100

    updated nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,8 +4,8 @@
       <h1 class="text-5xl text-lightC font-black my-auto ml-24">4PM</h1>
     </div>
     <div
-      class="float-right text-lighter h-full grid grid-cols-3 pr-32 text-center"
-    > 
+      class="float-right text-lighter h-full grid grid-cols-3 mr-32 text-center space-x-4"
+    >
       <div v-if="state" class="my-auto font-semibold text-2xl uppercase">
         <router-link to="/">Home</router-link>
       </div>
@@ -15,15 +15,15 @@
       <div v-if="!state" class="my-auto font-semibold text-2xl uppercase">
         <h1
           @click="logIn"
-          class="py-12 mx-4 hover:bg-bl-d hover:text-lighter cursor-pointer"
+          class="py-12 px-2  hover:bg-bl-d hover:text-lighter cursor-pointer"
         >
           Login
         </h1>
       </div>
-      <div  v-if="state" class="my-auto font-semibold text-2xl uppercase">
+      <div v-else class=" my-auto font-semibold text-2xl uppercase">
         <h1
           @click="logOut"
-          class="py-12 mx-4 hover:bg-bl-d hover:text-lighter cursor-pointer"
+          class="py-12 px-2 hover:bg-bl-d hover:text-lighter cursor-pointer"
         >
           LogOut
         </h1>
@@ -50,6 +50,7 @@ export default {
   computed: {
     ...mapGetters({
       state: "user/getloggedState",
+      userInfo: "user/getUserInfo",
     }),
   },
 };
--- src/views/ShowAll.vue
+++ src/views/ShowAll.vue
@@ -1,56 +1,72 @@
 <template>
   <base-card>
-    <h1 class="text-center text-lighter text-3xl font-bold mb-12 uppercase">
-      Projects List
-    </h1>
+    <div v-if="uid">
+      <h1 class="text-center text-lighter text-3xl font-bold mb-12 uppercase">
+        Projects List
+      </h1>
+      <div
+        class="text-center uppercase text-xl font-medium text-lighter"
+        v-if="err"
+      >
+        <base-icon
+          name="error"
+          h="1.5rem"
+          w="1.5rem"
+          class="inline-block mr-4 mb-2 animate-bounce fill-current text-burntSienna"
+        />
+        <span>ERROR while Loading Projects!!</span>
+        <base-icon
+          name="error"
+          h="1.5rem"
+          w="1.5rem"
+          class="inline-block ml-4 mb-2 animate-bounce fill-current text-burntSienna"
+        />
+      </div>
+      <div
+        class="text-center uppercase text-xl font-medium text-lighter"
+        v-else-if="loading"
+      >
+        <span class="text-xl">Loading Projects</span>
+        <base-icon
+          name="loading"
+          h="1.5rem"
+          w="1.5rem"
+          class="inline-block ml-4 mb-2 animate-spin fill-current text-lighter"
+        />
+      </div>
+      <div v-else-if="projects.length > 0">
+        <projects-viewer
+          v-for="project in projects"
+          @delete-project="deleteProject"
+          @delete-ptask="deleteTask"
+          @finished-ptask="finishedTask"
+          :key="project.id"
+          :pid="project.id"
+          :pname="project.name"
+          :pdesc="project.desc"
+          :ptasks="project.tasks ? project.tasks : null"
+          :pcustomW="project.completion"
+        ></projects-viewer>
+      </div>
 
-    <div
-      class="text-center uppercase text-xl font-medium text-lighter"
-      v-if="err"
-    >
-      <base-icon
-        name="error"
-        h="1.5rem"
-        w="1.5rem"
-        class="inline-block mr-4 mb-2 animate-bounce fill-current text-burntSienna"
-      />
-      <span>ERROR while Loading Projects!!</span>
-      <base-icon
-        name="error"
-        h="1.5rem"
-        w="1.5rem"
-        class="inline-block ml-4 mb-2 animate-bounce fill-current text-burntSienna"
-      />
+      <div
+        class="text-center uppercase text-xl font-medium text-lighter"
+        v-else
+      >
+        No items to show
+      </div>
     </div>
-    <div
-      class="text-center uppercase text-xl font-medium text-lighter"
-      v-else-if="loading"
-    >
-      <span class="text-xl">Loading Projects</span>
-      <base-icon
-        name="loading"
-        h="1.5rem"
-        w="1.5rem"
-        class="inline-block ml-4 mb-2 animate-spin fill-current text-lighter"
-      />
-    </div>
-    <div v-else-if="projects.length > 0">
-      <projects-viewer
-        v-for="project in projects"
-        @delete-project="deleteProject"
-        @delete-ptask="deleteTask"
-        @finished-ptask="finishedTask"
-        :key="project.id"
-        :pid="project.id"
-        :pname="project.name"
-        :pdesc="project.desc"
-        :ptasks="project.tasks ? project.tasks : null"
-        :pcustomW="project.completion"
-      ></projects-viewer>
-    </div>
-
-    <div class="text-center uppercase text-xl font-medium text-lighter" v-else>
-      No items to show
+    <div v-else>
+      <div class="text-center uppercase text-xl font-medium text-lighter">
+        <h1 class="text-center text-lighter text-3xl font-bold mb-12 uppercase">
+          Welcome to 4pm
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsum
+          magni eligendi repellat similique pariatur, aspernatur corrupti
+          cupiditate quisquam sapiente.
+        </p>
+      </div>
     </div>
   </base-card>
 </template>
@@ -68,7 +84,7 @@ export default {
       projects: "getProjects",
       loading: "getLoading",
       err: "getLoadingErr",
-      uid: 'user/getUID'
+      uid: "user/getUID",
     }),
   },
   methods: {
